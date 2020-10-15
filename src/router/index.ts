9d5c6c52d5508e034e11a6743f3fac0fbb53b3cf
qweroot <qweroot@pm.me>
2020-10-15 22:20:47 +0900

    add: views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,53 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '@/views/Dashboard.vue'
+import CategoryList from '@/views/CategoryList.vue'
+import CategoryDetail from '@/views/CategoryDetail.vue'
+import PartList from '@/views/PartList.vue'
+import ProductList from '@/views/ProductList.vue'
+import OrderList from '@/views/OrderList.vue'
+import TaskList from '@/views/TaskList.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    redirect: 'Dashboard'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/category',
+    name: 'CategoryList',
+    component: CategoryList
+  },
+  {
+    path: '/category/:categoryId',
+    name: 'CategoryDetail',
+    component: CategoryDetail
+  },
+  {
+    path: '/part',
+    name: 'PartList',
+    component: PartList
+  },
+  {
+    path: '/product',
+    name: 'ProductList',
+    component: ProductList
+  },
+  {
+    path: '/order',
+    name: 'OrderList',
+    component: OrderList
+  },
+  {
+    path: '/task',
+    name: 'TaskList',
+    component: TaskList
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/PartList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Part List</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
