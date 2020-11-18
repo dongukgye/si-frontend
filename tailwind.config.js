02954c7feb13a7ba092d2614ac690e3d0536ae85
qweroot <qweroot@pm.me>
2020-11-19 00:02:00 +0900

    add: tailwind custom forms package

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,24 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        '2xs': '0 0 20px 1px rgba(0, 0, 0, 0.05)'
+      }
+    },
+    customForms: theme => ({
+      semi: {
+        'checkbox': {
+          icon: '<svg fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" > <path d="M5 9 L6 8 H14 L15 9 V11 L14 12 H6 L5 11 Z"/></svg>'
+        }
+      }
+    })
   },
-  variants: {},
-  plugins: []
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    transitionProperty: ['responsive', 'hover', 'focus']
+  },
+  plugins: [
+    require('@tailwindcss/custom-forms')
+  ]
 }
