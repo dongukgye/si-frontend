1c4f4dff93524ebb2e10ea4b4746d5f7bb524d07
qweroot <qweroot@localhost.localdomain>
2020-11-28 17:30:55 +0900

    format: main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import '@/assets/css/tailwind.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@/assets/css/tailwind.css";
 
-import MainLayout from '@/layouts/MainLayout.vue'
+import MainLayout from "@/layouts/MainLayout.vue";
 
-createApp(App).component("default-layout", MainLayout).use(store).use(router).mount('#app')
+createApp(App)
+  .component("default-layout", MainLayout)
+  .use(store)
+  .use(router)
+  .mount("#app");
