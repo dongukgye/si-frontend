a9f368809452b6f8f2bd3c01e26033ecd765b3e2
user <user@macbook.local>
2020-12-12 18:12:05 +0900

    update: pagination component

--- src/components/pagination/Pagination.vue
+++ src/components/pagination/Pagination.vue
@@ -35,31 +35,36 @@
           </button>
         </div>
 
-        <div
-          v-show="openPageSizeDropdown"
-          :class="`origin-top-right absolute right-0 bottom-0 mb-12 w-full rounded-md shadow-2xs bg-white ring-1 ring-black ring-opacity-5 transform transition ${
-            openPageSizeDropdown
-              ? `opacity-100 scale-100 ease-out duration-100`
-              : `opacity-0 scale-95 ease-in duration-75`
-          }`"
+        <transition
+          enter-active-class="transition ease-out duration-100"
+          enter-from-class="transform opacity-0 scale-95"
+          enter-to-class="transform opacity-100 scale-100"
+          leave-active-class="transition ease-in duration-75"
+          leave-from-class="transform opacity-100 scale-100"
+          leave-to-class="transform opacity-0 scale-95"
         >
           <div
-            class="py-1"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
+            v-if="openPageSizeDropdown"
+            class="origin-top-right absolute right-0 bottom-0 mb-12 w-full rounded-md shadow-2xs bg-white ring-1 ring-black ring-opacity-5 transform transition"
           >
-            <a
-              v-for="(ps, i) in pageSizes"
-              :key="i"
-              href="#"
-              class="block px-4 py-2 text-xs hover:bg-gray-100 hover:text-gray-900"
-              role="menuitem"
-              @click="setPageSize(ps.value)"
-              >{{ ps.key }}</a
+            <div
+              class="py-1"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="options-menu"
             >
+              <a
+                v-for="(ps, i) in pageSizes"
+                :key="i"
+                href="#"
+                class="block px-4 py-2 text-xs hover:bg-gray-100 hover:text-gray-900"
+                role="menuitem"
+                @click="setPageSize(ps.value)"
+                >{{ ps.key }}</a
+              >
+            </div>
           </div>
-        </div>
+        </transition>
       </div>
 
       <div class="mr-4">
