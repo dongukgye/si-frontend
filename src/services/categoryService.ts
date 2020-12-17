76db30ae2536ccf3ca8bf9689a66026bbca51a5f
user <user@macbook.local>
2020-12-17 23:27:36 +0900

    update: category service

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,3 +1,4 @@
+import { ICategory } from '@/types/inventory';
 import httpClient from "./core/httpClient";
 import { IHttpRequest } from "./core/interfase";
 
@@ -9,6 +10,33 @@ class CategoryService {
       params: params,
     };
     return httpClient.request(axiosConfig);
+  }
+
+createCategory(data: ICategory): Promise<ICategory> {
+    const params: IHttpRequest<any> = {
+      path: "/inventory/categories/",
+      method: "post",
+      data: data,
+    };
+    return httpClient.request<ICategory>(params);
+  }
+
+  updateCategory(data: ICategory): Promise<ICategory> {
+    const params: IHttpRequest<any> = {
+      path: `/inventory/category/${data.id}/`,
+      method: "put",
+      data: data,
+    };
+    return httpClient.request(params);
+  }
+
+  deleteCategory(data: ICategory): Promise<ICategory> {
+    const params: IHttpRequest<any> = {
+      path: `/inventory/category/${data.id}/`,
+      method: "delete",
+      data: data,
+    };
+    return httpClient.request<ICategory>(params);
   }
 }
 
