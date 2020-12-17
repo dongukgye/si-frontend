d0a9eafe8f67368e8557e38d8b1c08425d14f0a2
user <user@macbook.local>
2020-12-17 23:27:44 +0900

    update: category list view

--- src/views/CategoryList.vue
+++ src/views/CategoryList.vue
@@ -1,34 +1,71 @@
 <template>
   <div>
     <Title title="Category List" />
-    <DataTable :headers="headers" :data="data" striped>
+    <DataTable :headers="headers" :data="items" selectable>
+      <template v-slot:col_category_status="{ item }">
+        <div class="flex items-center">
+          <div class="w-2 h-2 mx-2 bg-blue-300 rounded-full"></div>
+          {{ item.category_status }}
+        </div>
+      </template>
       <template v-slot:col_action="{ item }">
-        <button
-          @click="editItem(item)"
-          class="px-4 py-1 bg-indigo-500 hover:bg-indigo-800 text-white font-semibold rounded-md shadow-md"
-        >
-          EDIT
-        </button>
+        <div class="flex items-center">
+          <Button @click="startEditItem(item, editFunction)" text>
+            <icon :d="icons.pencilAlt" class="h-4 w-4" />
+          </Button>
+
+          <Button
+            @click="startDeleteItem(item, deleteFunction)"
+            text
+            type="danger"
+          >
+            <icon :d="icons.trash" class="h-4 w-4" />
+          </Button>
+        </div>
       </template>
     </DataTable>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import Title from "@/components/base/Title.vue";
+import { defineComponent, onMounted, ref } from "vue";
+import Button from "@/components/button/Button.vue";
 import DataTable from "@/components/table/DataTable.vue";
+import Icon from "@/components/icon/Icon.vue";
+import Title from "@/components/base/Title.vue";
+
+import categoryService from "@/services/categoryService";
+
+import { ICategory } from "@/types/inventory";
+import { useItemCrud } from "@/components/hooks/useItemCrud";
+import { usePagination } from "@/components/hooks/usePagination";
 
 export default defineComponent({
   components: {
-    Title,
+    Button,
     DataTable,
+    Icon,
+    Title,
   },
   setup() {
+    const icons = require("@/assets/icons.json");
+    const { totalCount } = usePagination();
+    const deleteFunction = ref(categoryService.deleteCategory);
+    const createFunction = ref(categoryService.deleteCategory);
+    const editFunction = ref(categoryService.deleteCategory);
+    const {
+      editedIndex,
+      editedItem,
+      items,
+      processEditItem,
+      processDeleteItem,
+      startEditItem,
+      startDeleteItem,
+    } = useItemCrud();
     const headers = ref([
-      { text: "Category", value: "category" },
-      { text: "Part Count", value: "count", sortable: true },
-      { text: "Status", value: "status" },
+      { text: "Category", value: "name" },
+      { text: "Part Count", value: "parts_count", editable: true },
+      { text: "Status", value: "category_status" },
       { text: "Action", value: "action" },
     ]);
     const data = ref([
@@ -36,9 +73,37 @@ export default defineComponent({
       { category: "Bolt", count: 310, status: "Lack" },
       { category: "Washaer", count: 28, status: "OK" },
     ]);
+
+    function fetchData(params: any) {
+      categoryService.getCategories(params).then(
+        (data: ICategory) => {
+          items.value = data;
+          totalCount.value = items.value.length;
+        },
+        (err: any) => {
+          console.log(err);
+        }
+      );
+    }
+
+    onMounted(() => {
+      fetchData({});
+      // fetchCategories();
+    });
+
     return {
+      deleteFunction,
+      createFunction,
+      editFunction,
       headers,
-      data,
+      editedIndex,
+      editedItem,
+      icons,
+      items,
+      processEditItem,
+      processDeleteItem,
+      startEditItem,
+      startDeleteItem,
     };
   },
 });
